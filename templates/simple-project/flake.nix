a6d1b37876f38b74ec52bf129e85230bf464c739
Jan Kouba <kouba.honza@gmail.com>
2020-12-11 09:19:48 +0100

    WIP

--- templates/simple-project/flake.nix
+++ templates/simple-project/flake.nix
@@ -3,13 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
   };
 
-  outputs = { self, nixpkgs }: {
-    foo = nixpkgs;
+  outputs = { self, nixpkgs } @inputs: {
+    foo = inputs;
 
     nixosConfigurations.dev-vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        import ./nix/modules/dev-machine.nix;
+        (import ./nix/modules/dev-machine.nix)
         "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
       ];
     };
